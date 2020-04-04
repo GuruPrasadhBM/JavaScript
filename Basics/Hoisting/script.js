77c3f210427ff94eeb67a981172e332681f04361
GuruBM <guruprasadh.mahalingam@gmail.com>
2020-04-04 16:34:25 +0530

    JS Basics

---
+++ Basics/Hoisting/script.js
@@ -0,0 +1,103 @@
+///////////////////////////////////////
+// Lecture: Hoisting
+
+//// Functions hoisting
+//Using the function before it is declared
+// The creation phase of the execution context, in this case the Global execution context. The function is created in the variable declaration object. Hence it is not mandatory to declare the function and use it.
+calculateAge(1982);
+
+function calculateAge(year)
+{
+    console.log(2020- year);
+}
+
+
+////Using Function Expression
+
+////In the below case we get Uncaught TypeError ////because the function expression is not initialized in during th creating phase of execution context.
+
+
+//retirement(1982);
+
+var retirement = function(year)
+{
+    console.log(65- (2020 - year));
+}
+
+
+
+//// Hoisting variables
+
+console.log(age); ////
+var age=23;
+//console.log(age); //// This Prints "undefined"
+
+function foo()
+{
+    var age = 65;
+    console.log(age);
+}
+
+foo();
+console.log(age);
+
+
+
+///////////////////////////////////////
+// Lecture: Scoping
+
+
+// First scoping example
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+*/
+
+
+
+// Example to show the differece between execution stack and scope chain
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
+
+function third() {
+    var d = 'John';
+    console.log(a + b + c + d);
+}
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: The this keyword
+
+
+
+
+
+
+
+
+
