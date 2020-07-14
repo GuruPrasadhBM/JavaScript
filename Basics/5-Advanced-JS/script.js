b669c0998de9f21dc935cd4e443fda78efe29020
GuruBM <guruprasadh.mahalingam@gmail.com>
2020-07-14 18:24:44 +0530

    Prototype in JS

---
+++ Basics/5-Advanced-JS/script.js
@@ -0,0 +1,22 @@
+var john = {
+  name: 'John',
+  yearOfBirth: 1990,
+  job: 'teacher',
+};
+
+//Function Constructor
+var Person = function (name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+};
+
+Person.prototype.calculateAge = function () {
+  console.log(2020 - this.yearOfBirth);
+};
+
+var john = new Person('John', 1990, 'teacher');
+john.calculateAge();
+
+var anotherPerson = new Person('John', 1890, 'teacher');
+anotherPerson.calculateAge();
