5bdf3c71ef2caa00f3231466e4b10e0c9c4d5e07
GuruBM <guruprasadh.mahalingam@gmail.com>
2020-07-15 14:57:14 +0530

    Functions as Objects

--- Basics/5-Advanced-JS/script.js
+++ Basics/5-Advanced-JS/script.js
@@ -23,7 +23,7 @@ anotherPerson.calculateAge();
 */
 
 //Object Create
-
+/*
 var personProto = {
   calculateAge: function () {
     console.log(2020 - this.yearOfBirth);
@@ -32,3 +32,30 @@ var personProto = {
 
 var john = Object.create(personProto);
 john.name = 'John';
+*/
+
+////Functions as objects
+
+var years = [1990, 1995, 2010, 2015];
+
+function arrayCalc(arr, fn) {
+  var arrResult = [];
+  for (i = 0; i < arr.length; i++) {
+    arrResult.push(fn(arr[i]));
+  }
+  return arrResult;
+}
+
+function CalculateAge(element) {
+  return 2020 - element;
+}
+
+function isFullAge(element) {
+  return element >= 18;
+}
+
+var ages = arrayCalc(years, CalculateAge);
+console.log(ages);
+
+var fullAges = arrayCalc(years, isFullAge);
+console.log(fullAges);
