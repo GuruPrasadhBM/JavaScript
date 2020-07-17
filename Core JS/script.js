46b32b29df4f6368c61dd995574946ea5262a6f5
GuruBM <guruprasadh.mahalingam@gmail.com>
2020-07-17 12:43:46 +0530

    Basics of arrays

--- Core JS/script.js
+++ Core JS/script.js
@@ -3,3 +3,19 @@ var arr = [10, 20, 30, 40];
 console.log(arr[4]);
 
 //Out of index returns undefined
+
+//Add element in an array
+//
+arr[20] = 6;
+console.log(arr[5]);
+console.log(arr[20]);
+console.log(arr[19]);
+console.log(arr.length);
+var newArr = arr;
+console.log(arr[20]);
+
+//Array[0] is converted to Array["0"] // Type coercion
+console.log(arr);
+
+arr['foo'] = 'bar'; // Will be added, but the length of the array
+//remains same
