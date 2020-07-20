5e63ec8e397c76436b1bc25de4b48a572fd8e75c
GuruBM <guruprasadh.mahalingam@gmail.com>
2020-07-20 18:26:32 +0530

    Completed Movie Booking app

--- VannilaJS/2MovieSeatBooking/script.js
+++ VannilaJS/2MovieSeatBooking/script.js
@@ -4,14 +4,53 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 console.log(ticketPrice);
+
+////Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
+
+  //1. Copy the selected seats into array
+  //2. Map through the array
+  //3. return a new array indexes
+  const seatsIndex = [...selectedSeats].map(function (seat) {
+    return [...seats].indexOf(seat);
+  });
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+  //console.log(seatsIndex);
+}
+
+//Get data from local storage and populate in UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  ////Check if the selected seats are not null
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        //Index available
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
 }
 
 ////Click on a Seat to change it to get selected
@@ -29,5 +68,9 @@ container.addEventListener('click', (e) => {
 ////Movie Selection Changed Event
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
+
+////Initial count and total setting
+updateSelectedCount();
