1571172ddef4bb25e289935382a5f4bc7827faff
GuruBM <guruprasadh.mahalingam@gmail.com>
2020-08-03 13:49:44 +0530

    Intro to promises

---
+++ Core JS/features.js
@@ -0,0 +1,22 @@
+////Promises
+
+let promiseToCleanTheRoom = new Promise(function (resolve, reject) {
+  ////Do the cleaning process
+  let isClean = false;
+
+  if (isClean) {
+    resolve('cleaned');
+  } else {
+    reject('not Cleaned');
+  }
+});
+
+promiseToCleanTheRoom
+  .then(function (fromResolve) {
+    ////Room Cleaned
+    console.log('The room is ' + fromResolve);
+  })
+  .catch(function (fromReject) {
+    console.log('The room is ' + fromReject);
+    ////Room not cleaned
+  });
